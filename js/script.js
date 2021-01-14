06eb9d783f163adfca3f389e2322ee8356b3c73b
barkasOff <boris.sizov.2001@mail.ru>
2021-01-14 11:50:10 +0300

    Slider's easy-realization is completed!
    Закончена реализация "легкого" слайдера. Он был реализован при помощи
    наиболее простого метода: добавление в classList класса "hide", а также
    удаление его от туда (класс ставит свойство элемента display в значение
    none).

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    alert("Hello");
+    const   showSlide = (n) => {
+        if (n > slides.length) {
+            slideI = 1;
+        }
+        if (n < 1) {
+            slideI = slides.length;
+        }
+
+        slides.forEach(item => item.classList.add('hide'));
+        slides[slideI - 1].classList.remove('hide');
+    };
+    const   initSlide = (n, parent) => {
+        if (n > 9) {
+            parent.textContent = n;
+        } else {
+            parent.textContent = `0${n}`;
+        }
+    };
+    const   slides = document.querySelectorAll('.easy-slider__slide'),
+            prev = document.querySelector('.easy-slider__left'),
+            next = document.querySelector('.easy-slider__right'),
+            cur = document.querySelector('#easy-slider__cur'),
+            total = document.querySelector('#easy-slider__total');
+    let     slideI = 1;
+
+    showSlide(slideI);
+    initSlide(slideI, cur);
+    initSlide(slides.length, total);
+    prev.addEventListener('click', () => {
+        --slideI;
+        showSlide(slideI);
+        initSlide(slideI, cur);
+    });
+    next.addEventListener('click', () => {
+        ++slideI;
+        showSlide(slideI);
+        initSlide(slideI, cur);
+    });
 });
