7be704b9dbbc32febb9d7610f6b0244077fbbccb
barkasOff <boris.sizov.2001@mail.ru>
2021-01-20 00:03:00 +0300

    Structure for navigation in hard-slider
    Сделана стурктура для навигации в реализвации сложного слайдера

--- src/js/modules/hard-slider.js
+++ src/js/modules/hard-slider.js
@@ -6,14 +6,28 @@ const hardSlider = () => {
             next = document.querySelector('#hs-right'),
             cur = document.querySelector('#hs-cur'),
             total = document.querySelector('#hs-total'),
+            hardSlider = document.querySelector('.hard-slider'),
             hardSliderParent = document.querySelector('.hard-slider__wrapper'),
             hardSliderField = document.querySelector('.hard-slider__inner'),
             width = window.getComputedStyle(hardSliderParent).width;
+    const   hardSliderNav = document.createElement('ol');
     let     slideI = 1,
             offset = 0;
 
     initSlide(slideI, cur);
     initSlide(slides.length, total);
+    hardSliderNav.classList.add('hard-slider__nav');
+    for (let i = 0; i < slides.length; ++i) {
+        const   nav = document.createElement('li');
+
+        nav.setAttribute('data-slide-to', i + 1);
+        nav.classList.add('hard-slider__nav_el');
+        if (i == 0) {
+            nav.classList.add('hard-slider__nav_el-active');
+        }
+        hardSliderNav.append(nav);
+    }
+    hardSlider.append(hardSliderNav);
     hardSliderField.style.width = `${100 * slides.length}%`;
     hardSliderField.style.display = 'flex';
     hardSliderField.style.transition = '0.5s all';
